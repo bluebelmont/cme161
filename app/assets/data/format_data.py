e5c76f50ab825f7a427fae40f96392fd15eadc8c
Blue Sheffer <bluesheffer@DN0a2570d1.SUNet>
2016-06-01 11:44:51 -0700

    start/finish backend tutorial

---
+++ app/assets/data/format_data.py
@@ -0,0 +1,54 @@
+import json
+import scipy.io as sio
+import numpy as np
+
+def main():
+	conditions = ['30', '60', '100', 'Hold']
+	for cond in range(len(conditions)):
+		left_filename = 'jumpAt' + conditions[cond] + 'left.mat'
+		right_filename = 'jumpAt' + conditions[cond] + 'right.mat'
+		mat_contents_left = sio.loadmat(left_filename)
+		mat_contents_right = sio.loadmat(right_filename)
+		left = mat_contents_left['lowDXleft']
+		right = mat_contents_right['lowDXright']
+
+		num_left_trials, T, dim = np.shape(left)
+		num_right_trials, _, _ = np.shape(right)
+		d = {}
+		all_time_dict = {}
+		for t in range(T):
+			t_dict = {}
+			all_trial_dict = {}
+			for trial in range(num_left_trials):
+				x = left[trial, t, 0]
+				y = left[trial, t, 1]
+				z = left[trial, t, 2]
+				if not np.isnan(x):
+					trial_dict = {}
+					trial_dict['direction'] = 'left'
+					trial_dict['x'] = x
+					trial_dict['y'] = y
+					trial_dict['z'] = z
+					all_trial_dict[trial] = trial_dict
+			for trial in range(num_right_trials):
+				x = right[trial, t, 0]
+				y = right[trial, t, 1]
+				z = right[trial, t, 2]
+				if not np.isnan(x):
+					trial_dict = {}
+					trial_dict['direction'] = 'right'
+					trial_dict['x'] = x
+					trial_dict['y'] = y
+					trial_dict['z'] = z
+					all_trial_dict[trial + num_left_trials] = trial_dict
+			t_dict['trial'] = all_trial_dict
+			all_time_dict[t] =  t_dict
+		d['time'] = all_time_dict
+		with open(conditions[cond] + 'data.js', 'w') as outfile:
+			json.dump(d, outfile)
+
+
+	
+
+if __name__ == "__main__":
+	main()
--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -1,4 +1,5 @@
 import os, copy
+import json, collections
 from flask import Flask, jsonify, request, send_from_directory, make_response
 app = Flask(__name__, static_url_path='')
 
@@ -21,9 +22,51 @@ def send_assets_html(path):
 	print file
 	return file
 
+@app.route('/trellis', methods=['GET'])
+def get_trellis():
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(json.load(data_file))
+
+@app.route('/trellis/limit/<int:n_entries>', methods=['GET'])
+def get_trellis_limit(n_entries):
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(json.load(data_file)[:n_entries])
+
+def make_data_graph(data_list_in):
+	idx = 0
+	names = collections.OrderedDict()
+	for e in data_list_in:
+		to = e['to'][:7] # truncate
+		fr = e['from'][:7]
+		if to not in names:
+			names[to] = idx
+			idx += 1
+		if fr not in names:
+			names[fr] = idx
+			idx += 1
+	edges = [{
+				"source": names[e['to'][:7]], 
+				"target": names[e['from'][:7]], 
+				"value": e['n'], 
+				"tags":  [d['tag'] for d in e['data']] 
+			} for e in data_list_in
+		]
+	nodes = [{"name":n} for n in names.keys()]
+	return { "nodes": nodes, "edges": edges }
+
+@app.route('/graph', methods=['GET'])
+def get_graph():
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(make_data_graph(json.load(data_file)))
+
+@app.route('/graph/limit/<int:n_entries>', methods=['GET'])
+def get_graph_limit(n_entries):
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(make_data_graph(json.load(data_file)[:n_entries]))
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5050))
-	app.run(host='0.0.0.0', port=port, debug=False)
+	app.run(host='0.0.0.0', port=port, debug=True)
 
 # set debug=True if you want to have auto-reload on changes
 # this is great for developing
